9fa98e68037c86c56b449919d317cdce19af14a5
Marty McGee <mcgee.marty@gmail.com>
2026-06-05 11:06:48 -0700

    v0.1.22: Better Auth

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -3,6 +3,6 @@ import { auth } from '@/lib/auth/server';
 import { toNextJsHandler } from "better-auth/next-js";
 
 // Ensure Node.js runtime (not edge)
-// export const runtime = 'nodejs';
+export const runtime = 'nodejs';
 
 export const { GET, POST } = toNextJsHandler(auth);
