614cbf1c9964edf2e2098ca347151df78c0adcb2
Marty McGee <mcgee.marty@gmail.com>
2026-07-09 09:55:22 -0700

    v0.4.2: User > Project > Module Heirarchy w Many Children + Auth.js + Favicon

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,14 +4,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { ThemeSelector } from "@/components/themes/selector";
 import { Button } from "@/components/ui/button";
-import { useSession, signOut } from "@/lib/auth/better-auth/client";
+import { 
+  auth,
+  // useSession, 
+  // getSession,
+  signOut 
+} from "@/lib/auth";
 
 export function NavBar() {
-  const { data: session, isPending } = useSession();
+  // const { data: session, isPending } = useSession();
 
+  // const session = auth();
+  const session = async () => {
+    await auth();
+  };
+  
   const handleSignOut = async () => {
     await signOut();
   };
+
+  const isPending = session?.user?.id ? false : true
 
   return (
     <nav className="flex items-center justify-between py-2 md:py-2">
@@ -19,16 +31,16 @@ export function NavBar() {
         <div className="flex items-center">
           <Image
             className="lg:h-7 lg:w-auto dark:hidden"
-            src="/logo.svg"
-            alt="Neon logo"
+            src="/favicon.png"
+            alt="App logo"
             width={88}
             height={24}
             priority
           />
           <Image
             className="hidden lg:h-7 lg:w-auto dark:block"
-            src="/logo-dark.svg"
-            alt="Neon logo"
+            src="/favicon.png"
+            alt="App logo"
             width={88}
             height={24}
             priority
